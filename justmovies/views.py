43ed77e30f4efda34d32356af23ff8bcbeae5a24
Mehta <shashwat.black@gmail.com>
2018-10-05 13:19:59 -0500

    Django can talk to db_utils.

--- justmovies/views.py
+++ justmovies/views.py
@@ -1,6 +1,13 @@
 from django.http import HttpResponse
 from django.views import View
 
+from utils.db_utils import DatabaseUtils
+
 class HomeView(View):
     def get(self, request):
-        return HttpResponse('result')
+        db = DatabaseUtils()
+        query = """SELECT * FROM country;"""
+        db.get_cursor().execute(query);
+        return HttpResponse(
+            db.get_cursor().fetchall()
+        )
