6dd3c775d27e17258bab9fa5e09ba5d16bd2af5c
Shashwat Black <shashwat.black@gmail.com>
2018-10-07 18:14:51 -0500

    More data for Wil Wheaton.

--- data/custom_statements.sql
+++ data/custom_statements.sql
@@ -8,3 +8,12 @@ King has received Bram Stoker Awards, World Fantasy Awards, and British Fantasy 
 
 Source: wikipedia.org/wiki/Stephen_King'
 WHERE NAME = 'Stephen King';
+
+UPDATE person
+SET
+  image = 'https://wallscover.com/images/wil-wheaton-wallpaper-2.jpg',
+  dob = '07/21/1972',
+  intro = 'Wil Wheaton was born Richard William Wheaton III on July 29, 1972 in Burbank, California. He first gained international attention by starring in the Rob Reiner comedy-drama film Stand by Me (1986). He then played Wesley Crusher on the television series Star Trek: The Next Generation (1987) for three and one-quarter seasons. Wheaton left the Hollywood scene for 18 months to pursue personal video production. He did return to "Star Trek" every now and then for an occasional episode, however. He then returned to Los Angeles, California, attended acting school for five years and now works on many projects. He lives in Arcadia, California with his wife Anne and her two sons.
+
+- IMDb Mini Biography By: Matt Dicker'
+WHERE NAME = 'Wil Wheaton';
